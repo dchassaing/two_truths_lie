2042d1f08a9354c92dace99c517baec789c0a264
dchassaing <delia.chassaing@gmail.com>
2017-05-10 19:28:40 -0500

    reset functions and wrap around text

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 
 $(document).ready(function(){
 
- var lastClicked = '';
-
   $('.option-2').click(function(){
 
     $('.option-2').css("border", "green solid 1px");
@@ -14,9 +12,11 @@ $(document).ready(function(){
   });
   $('.option-1').click(function(){
     wrongAnswer('.option-1');
+    setTimeout(reset1, 1500);
   });
   $('.option-3').click(function(){
     wrongAnswer('.option-3');
+    setTimeout(reset2, 1500);
   });
 
   function wrongAnswer (option) {
@@ -28,9 +28,21 @@ $(document).ready(function(){
     $('.prompt').css("text-align", "center");
   }
 
-  function reset () {
-  // if option has been clicked on
-  //undo border 
+  function reset1() {
+    $('.option-1').css("border", "#E8D284 solid 1px");
+    $('.option-1').css("color", "black");
+    $('.option-1').css("background-color", "white");
+    $('.prompt').html("Guess the lie...");
+    $('.prompt').css("color", "black");
+    $('.prompt').css("text-align", "left");
   }
 
+  function reset2() {
+    $('.option-3').css("border", "#E8D284 solid 1px");
+    $('.option-3').css("color", "black");
+    $('.option-3').css("background-color", "white");
+    $('.prompt').html("Guess the lie...");
+    $('.prompt').css("color", "black");
+    $('.prompt').css("text-align", "left");
+  }
 });
